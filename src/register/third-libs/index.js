d9da3cb3d53dfdb3b1102616e2a760e85dc85f91
ZhengJunChan <zhengjunchan@qq.com>
2017-12-25 15:57:05 +0800

    注册http插件 和 样式插件

--- src/register/third-libs/index.js
+++ src/register/third-libs/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 
 import vuex from 'vuex'
+import { AjaxPlugin } from 'vux'
+
+// 添加样式插件
 Vue.use(vuex)
 
-// 添加http拦截
-import { AjaxPlugin } from 'vux';
-Vue.use(AjaxPlugin);
+// 添加http插件
+Vue.use(AjaxPlugin)
